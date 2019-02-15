8fdf4a3330a8b3df80e8b1558508240eb5306f17
daniela amyot <daniela.amyot@gmail.com>
2019-02-15 16:54:21 -0500

    banner

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'cocktails/show'
   get 'cocktails/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+root to:'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:new, :create, :show, :index] do
     resources :doses, only: [:create, :new]
